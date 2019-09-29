5d52df13730dc9d79f899ea5a64e7173a0d01a75
Martin Whitaker <martin@stickman.co.uk>
2019-09-29 10:55:33 +0100

    Use regs instead of wires. Not sure if this is good? Seems to make routing easier

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # verilog_ws2811
+# *** This is a work in progress as I teach myself Verilog ***
+# The project goal is to create a Verilog based controller for WS2811 LED strips
+# with various built-in effects, such as rainbows, wipes and sparkles
+# (similar to the FastLED library in Arduino).
+# Note: I am very much a beginner - this code is probably flawed in many ways!
--- controller.v
+++ controller.v
@@ -11,43 +11,72 @@ endmodule
 
 module ledcontroller (input clk, input [7:0] mode, input [7:0] ledindex, input [7:0] animationcounter, input [7:0] stepclock, output reg [7:0] red, output reg [7:0] green, output reg [7:0] blue);
 
-	wire [1:0] colindex;
+	reg [1:0] colindex;
 
-	wire [15:0] fractionalposition;
-	wire [15:0] proxa;
-	wire [7:0] proximity;
+	reg [15:0] fractionalposition;
+	reg [15:0] proxa;
+	reg [7:0] proximity;
 
-	assign colindex[1:0] = stepclock[1:0] + ledindex;
-	assign fractionalposition=animationcounter*49; // Number of leds in the string goes here
-	assign proxa[15:0] = (fractionalposition>{ ledindex, 8'h00}) ? (fractionalposition-{ ledindex, 8'h00} ) : ({ ledindex, 8'h00}-fractionalposition);
-	assign proximity[7:0] = (proxa>=1024) ? 0 : (proxa<=8) ? 255: 256-(proxa/4);
+	reg [7:0] steppedcol_red;
+	reg [7:0] steppedcol_green;
+	reg [7:0] steppedcol_blue;
+
+	// Generate the non-clocked logic for faded proximity intensities
+	always @* begin
+		// Generate the fractional position 
+		fractionalposition[15:0] <= animationcounter*49;
+		// ABS type function
+		if (fractionalposition>{ ledindex, 8'h00}) begin
+			proxa[15:0] <= fractionalposition-{ ledindex, 8'h00};
+		end else begin
+			proxa[15:0] <= { ledindex, 8'h00}-fractionalposition;
+		end
+		// Actually generate the fading relative to the animation position
+		if (proxa>=1024) begin
+			proximity[7:0] <= 0;
+		end else begin
+			if (proxa<=8) begin
+				proximity[7:0] <= 255;
+			end else begin
+				proximity[7:0] <= 256-(proxa/4);
+			end
+		end
+	end
+
+	// Generate the non-clocked logic for building stepped colours
+	always @* begin
+		colindex[1:0] <= stepclock[1:0] + ledindex;
+		case (colindex)
+			0: begin
+				steppedcol_red[7:0] <= (8'hFF);
+				steppedcol_green[7:0] <= (8'h00);
+				steppedcol_blue[7:0] <= (8'h00);
+			end
+			1: begin
+				steppedcol_red[7:0] <= (8'h00);
+				steppedcol_green[7:0] <= (8'hFF);
+				steppedcol_blue[7:0] <= (8'h00);
+			end
+			2: begin
+				steppedcol_red[7:0] <= (8'h00);
+				steppedcol_green[7:0] <= (8'h00);
+				steppedcol_blue[7:0] <= (8'hFF);
+			end
+			3: begin
+				steppedcol_red[7:0] <= (8'hFF);
+				steppedcol_green[7:0] <= (8'hFF);
+				steppedcol_blue[7:0] <= (8'h00);
+			end
+		endcase		
+	end
 
 	always @(posedge clk) begin
 		case (mode)
 			0: begin
 				// Red, Green, Blue, Yellow stepped
-				case (colindex)
-					0: begin
-						red[7:0] <= (8'hFF);
-						green[7:0] <= (8'h00);
-						blue[7:0] <= (8'h00);
-					end
-					1: begin
-						red[7:0] <= (8'h00);
-						green[7:0] <= (8'hFF);
-						blue[7:0] <= (8'h00);
-					end
-					2: begin
-						red[7:0] <= (8'h00);
-						green[7:0] <= (8'h00);
-						blue[7:0] <= (8'hFF);
-					end
-					3: begin
-						red[7:0] <= (8'hFF);
-						green[7:0] <= (8'hFF);
-						blue[7:0] <= (8'h00);
-					end
-				endcase
+				red[7:0] <= steppedcol_red[7:0];
+				green[7:0] <= steppedcol_green[7:0];
+				blue[7:0] <= steppedcol_blue[7:0];
 			end
 			1: begin
 				// Faded block running up
